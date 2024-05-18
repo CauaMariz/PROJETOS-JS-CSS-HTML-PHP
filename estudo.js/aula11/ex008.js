b6d321d84fc5d577594c2041a95cf0c51d785cee
CM-FSTCK <marizcaua88@gmail.com>
2024-05-18 20:21:38 -0300

    Delete estudo.js directory

--- estudo.js/aula11/ex008.js
+++
@@ -1,6 +0,0 @@
-var v=60
-
-if(v>60){
-    console.log(`voce foi multado, pois sua velocidade estava em ${v}KM/H`)
-}
-console.log(`a velocidade de seu carro é de ${v}KM/H`)
--- estudo.js/aula11/ex009.js
+++
@@ -1,7 +0,0 @@
-var p='japao'
-console.log(`'O pais é ${p}`)    
-if (p=='BRASIL'){
-    console.log('voce é brasileiro')
-} else{
-    console.log('voce nao é brasileiro')
-}
