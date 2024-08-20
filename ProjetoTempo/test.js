f1cc42490a5020f0a084e7f9d1476bc0051b394b
Caua Mariz DEV <marizcaua88@gmail.com>
2024-08-20 11:34:24 -0300

    Update test.js

--- ProjetoTempo/test.js
+++ ProjetoTempo/test.js
@@ -1,6 +1,7 @@
 
 let campo = document.querySelector('#campo')
 let x = false
+
 async function tempo() {
     try {
         const url = await fetch('http://api.weatherapi.com/v1/current.json?key=055b0e7a30f3463fa18142639241308&q=Sao Paulo&aqi=yes')
@@ -13,7 +14,7 @@ async function tempo() {
                 const temperatura = ver.current.temp_c;
                 const hora = ver.location.localtime;
                 const vento = ver.current.wind_mph;
-                
+
                 campo.style.fontFamily = 'Arial'
                 campo.style.maxWidth = '260px'
                 campo.style.backgroundColor = 'rgb(34, 126, 34)'
@@ -21,6 +22,8 @@ async function tempo() {
                 campo.style.fontSize = '20px'
                 campo.style.padding = '20px'
                 campo.style.borderRadius = '10px'
+                campo.style.lineHeight='6vh'
+                
                 campo.innerHTML = ('Temperatura atual: ' + temperatura + 'C ' + '<br>' + 'Hora atual: ' + hora + '<br>' + 'Velocidade do vento em MPH: ' + vento + 'MPH')
 
                 let btn = document.createElement('button')
@@ -41,4 +44,4 @@ async function tempo() {
     catch (error) {
         campo.innerHTML = (error)
     }
-}
+}   
