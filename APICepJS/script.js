e3774c4251040f7ec315ade0d952c3fac903348a
Caua Mariz DEV <marizcaua88@gmail.com>
2024-09-07 20:15:32 -0300

    Update script.js

--- APICepJS/script.js
+++ APICepJS/script.js
@@ -1,48 +1,81 @@
-let campo = document.querySelector("#campo")
+let campo = document.querySelector("#campo");
+let cepp = document.querySelector("#cep");
 
-let cepp = document.querySelector("#cep")
+let x = false;
 
-x = false
 function cep() {
-
-    let btn = document.createElement("button")
-    btn.id = 'btn'
-    btn.textContent = 'Ver Cep'
-
     if (!x) {
+        let btn = document.createElement("button");
+        btn.id = 'btn';
+        btn.textContent = 'Ver Cep';
 
-        document.body.appendChild(btn)
+        document.body.appendChild(btn);
 
-        btn.addEventListener('click', async () => {
+        if (cepp.value.length < 8 || cepp.value.length > 8) {
 
-            const url = await fetch('https://viacep.com.br/ws/05576100/json/')
-            const urljson = await url.json()
-
-            if (url.ok) {
-
-                if (campo.classList.contains("Close")) {
-
-                    campo.classList.remove("Close")
-                    campo.classList.add("Ver")
-
-                    setTimeout(() => {
-                        campo.innerHTML = 'Nome  do seu Cep: ' + cepp.value + JSON.stringify(urljson.logradouro)
-                    }, 330)
-                }
-                else {
-                    campo.innerHTML = ''
-                    setTimeout(() => {
-                        campo.classList.remove("Ver")
-                        campo.classList.add("Close")
-                    }, 100)
-                }
-
-            }
+            campo.classList.add('Ver')
+            campo.innerHTML = 'Digite um CEP de 8 caracteres'
+            btn.textContent = 'Recarregar pagina'
+            btn.addEventListener('click', () => {
+                location.reload()
+            })
         }
+        else {
 
-        )
+            btn.addEventListener('click', async () => {
 
-        x = true
+                const url = await fetch(`https://viacep.com.br/ws/${cepp.value}/json/`);
+                const urljson = await url.json();
+
+                if (urljson.erro == 'true') {
+                    if (campo.classList.contains("Close")) {
+                        campo.classList.remove("Close");
+                        campo.classList.add("Ver");
+
+                        setTimeout(() => {
+                            campo.innerHTML = 'Nao foi possivel localizar este CEP. Digite novamente.';
+                        }, 330);
+                    }
+
+                    else {
+                        campo.innerHTML = '';
+                        setTimeout(() => {
+                            campo.classList.remove("Ver");
+                            campo.classList.add("Close");
+                        }, 100);
+                    }
+
+
+                    btn.textContent = 'Recarregar página';
+                    btn.addEventListener('click', () => {
+                        location.reload();
+                    });
+                }
+
+                else {
+                    if (url.ok && !urljson.erro) {
+                        if (campo.classList.contains("Close")) {
+                            campo.classList.remove("Close");
+                            campo.classList.add("Ver");
+
+                            setTimeout(() => {
+                                campo.innerHTML = 'Número do seu CEP: ' + '<b>' + cepp.value + '</b>' + '<br>' + '. Rua: ' + '<b>' + JSON.stringify(urljson.logradouro) + '</b>' + '<br>' + '. Bairro: ' + '<b>' + JSON.stringify(urljson.bairro) + '</b>' + '<br>' + '. Cidade: ' + '<b>' + JSON.stringify(urljson.localidade) + '</b>' + '<br>' + '. Estado: ' + '<b>' + JSON.stringify(urljson.estado) + '</b>';
+                            }, 330);
+                        }
+
+                        else {
+                            campo.innerHTML = '';
+                            setTimeout(() => {
+                                campo.classList.remove("Ver");
+                                campo.classList.add("Close");
+                            }, 100);
+                        }
+                    }
+
+                }
+            });
+
+        }
+        x = true;
     }
 }
-
