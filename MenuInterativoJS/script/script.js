76ff9b6ba7f0756d5ffade9a30e573b8418bf530
Caua Mariz DEV <marizcaua88@gmail.com>
2024-09-08 13:16:53 -0300

    Add files via upload

---
+++ MenuInterativoJS/script/script.js
@@ -0,0 +1,80 @@
+control = false
+let campo = document.querySelector('#campo')
+
+
+
+function Home() {
+  if (control) {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_read_notes_gqmq.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+
+    control = true
+  }
+  else {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_read_notes_gqmq.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+  }
+}
+
+function Login() {
+  if (control) {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_Pic_profile_re_7g2h.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+
+    control = true
+  }
+  else {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_Pic_profile_re_7g2h.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+  }
+}
+
+function Cadastro() {
+  if (control) {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_undraw_undraw_undraw_sign_up_ln1s_-1-_s4bc_-1-_ee41_(1)_kf4d.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+
+    control = true
+  }
+  else {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_undraw_undraw_undraw_sign_up_ln1s_-1-_s4bc_-1-_ee41_(1)_kf4d.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+  }
+}
+
+function Compras() {
+  if (control) {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_undraw_undraw_undraw_selected_box_09k4_-1-_jnk6_9unq_(1)_37xp.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+
+    control = true
+  }
+  else {
+    let img = document.createElement('img')
+    img.src = 'imgs/undraw_undraw_undraw_undraw_selected_box_09k4_-1-_jnk6_9unq_(1)_37xp.png'
+    campo.innerHTML = ''
+
+    campo.appendChild(img)
+  }
+}
