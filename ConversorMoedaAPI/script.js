cdbd950990b35ecb4481af771a0999e312625e61
Caua Mariz DEV <marizcaua88@gmail.com>
2024-09-21 19:36:13 -0300

    Update script.js

--- ConversorMoedaAPI/script.js
+++ ConversorMoedaAPI/script.js
@@ -1,42 +1,34 @@
 async function See() {
   try {
-    let campo = document.querySelector("#campo")
-    campo.classList.toggle('Exibir')
+    let campo = document.querySelector("#campo");
 
-    let urlawait = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
-    let urljson = await urlawait.json()
 
+    let urlawait = await fetch('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL');
+    let urljson = await urlawait.json();
+    let dolar = parseFloat(urljson.USDBRL.high);
+    let real = document.querySelector('#real').value;
+    let conversao = (real / dolar).toFixed(2);
+
+    if (!real && !campo.classList.contains('Exibir')) {
+      return;
+    }
 
     if (urlawait.ok) {
+      campo.classList.toggle('Exibir');
       if (campo.classList.contains('Exibir')) {
-        campo.style.width = '300px'
-        campo.style.height = '100px'
-        campo.style.transition = '0.6s'
-        campo.style.backgroundColor = 'rgb(190, 190, 190)'
-        campo.style.borderRadius = '10px'
-        campo.style.padding='10px 0 0 10px'
-        campo.style.color='white'
-        campo.style.fontSize='17px'
-        campo.style.marginTop='2vh'
-        campo.style.fontFamily='system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-seri'
-        campo.style.marginLeft='1%'
-        campo.style.fontWeight='600'
+        
+        setTimeout(() => {
+          campo.innerHTML = `R$${real} em ${dolar.toFixed(2)} sao: U$$${conversao}`;
+        }, 400)
 
-      setTimeout(()=>{
-          campo.innerHTML = "1 Dolar em real: " + 'R$' + JSON.stringify(urljson.USDBRL.high)
-        },400)
       }
       else {
-        campo.innerHTML = ''
-        campo.style.width = '0'
-        campo.style.height = '0'
-        campo.style.transition = '0.6s'
-        campo.style.padding='0'
+        campo.innerHTML = '';
       }
     }
   }
 
-  catch (erro) {
-    document.write(erro)
+  catch  {
+    campo.innerHTML = "Algo nao está certo";
   }
 }
