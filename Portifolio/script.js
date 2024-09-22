5eccf9ccab61189f66f858be65968c4f97bcdd61
Caua Mariz DEV <marizcaua88@gmail.com>
2024-09-22 19:17:41 -0300

    Update script.js

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -12,6 +12,9 @@ function Fechar() {
   else {
     btn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill" viewBox="0 0 16 16"><path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708"/></svg>'
 
-    BarraLateral.innerHTML = ' <span>Veja também</span><a href="https://www.linkedin.com/in/caua-mariz-254b8731a/" target="_blank"><img src="imgs/icons8-linkedin-48.png" alt=""></a><a href="https://github.com/CauaMariz" target="_blank"><img src="imgs/icons8-github-50.png" alt=""></a><a href="https://www.instagram.com/cmarizzzz/?igsh=eTk1MzdvZm83M2Nr" target="_blank"><img src="imgs/icons8-instagram-48.png" alt=""></a><h2>Projetos</h2><a href="https://github.com/CauaMariz/PROJETOS-JS-CSS-HTML-PHP/tree/1c1e691306cd985ff0d47743cc19379e1d3c64d7/calculadora" target="_blank"><img src="imgs/icons8-calculadora-de-maçã-48.png" alt=""></a>'
+    BarraLateral.innerHTML = ' <span>Veja também</span><a href="https://www.linkedin.com/in/caua-mariz-254b8731a/" target="_blank"><img src="imgs/icons8-linkedin-48.png" alt=""></a><a href="https://github.com/CauaMariz" target="_blank"><img src="imgs/icons8-github-50.png" alt=""></a><a href="https://www.instagram.com/cmarizzzz/?igsh=eTk1MzdvZm83M2Nr" target="_blank"><img src="imgs/icons8-instagram-48.png" alt=""></a><h2>Projetos</h2><a href="https://github.com/CauaMariz/PROJETOS-JS-CSS-HTML-PHP/tree/1c1e691306cd985ff0d47743cc19379e1d3c64d7/calculadora" target="_blank"><img src="imgs/icons8-calculadora-de-maçã-48.png" alt=""></a> <a href="https://github.com/CauaMariz/PROJETOS-JS-CSS-HTML-PHP/tree/main/APICepJS" target="_blank"><img src="imgs/icons8-mapa-48.png" alt=""></a><a href="https://github.com/CauaMariz/PROJETOS-JS-CSS-HTML-PHP/tree/main/ConversorMoedaAPI" target="_blank"><img src="imgs/icons8-barato-2-48.png" alt=""></a>'
   }
+}
+function SeeProjectsContt(){
+  location.href='projts.html'
 }
