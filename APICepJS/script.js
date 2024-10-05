6197277056f89322cc672fcf67bf61f406d9ebb3
Caua Mariz DEV <marizcaua88@gmail.com>
2024-10-04 22:39:59 -0300

    Update script.js

--- APICepJS/script.js
+++ APICepJS/script.js
@@ -1,67 +1,50 @@
 
 //Declaracao de VARs.
-let campo = document.querySelector('#campo')
-let cep = document.querySelector("#cep")
+let campo = document.querySelector('#campo');
+let cep = document.querySelector("#cep") ?? "";
+
 //
+
+function ExibirMensagem(mensagem) {
+    campo.innerHTML = campo.classList.contains('Ver') ? mensagem : "";
+}
 
 //function asincrona.
 async function VerCep() {
 
     //Verficacao se o input for undefined.
-    if (cep.value == '') {
-        if (campo.classList.contains('Ver')) {
-            campo.innerHTML = 'É necessario preencher o campo "CEP"'
-        }
-        else { campo.innerHTML = '' }
+    if (cep.value === '') {
+        ExibirMensagem("É necessário preencher o campo \"CEP\".");
+        return;
     }
     //
+    try {
+        //Parte do URL.
+        const url = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`);
+        const urljson = await url.json();
+        //
 
-    else {
-        try {
+        //Verificaca caso a url seja veridica.
+        if (url.ok && !urljson.erro) {
 
-            //Parte do URL.
-            const url = await fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
-            const urljson = await url.json()
+            //Exibicao do resultado.
+            ExibirMensagem('Número do seu CEP: ' + '<b>' + cep.value + '</b>' + '<br>' + '. Rua: ' + '<b>' + JSON.stringify(urljson.logradouro) + '</b>' + '<br>' + '. Bairro: ' + '<b>' + JSON.stringify(urljson.bairro) + '</b>' + '<br>' + '. Cidade: ' + '<b>' + JSON.stringify(urljson.localidade) + '</b>' + '<br>' + '. Estado: ' + '<b>' + JSON.stringify(urljson.estado) + '</b>');
             //
-
-            //Verificaca caso a url seja veridica.
-            if (url.ok && !urljson.erro) {
-
-                if (campo.classList.contains('Ver')) {
-                    //Exibicao do resultado.
-                    campo.innerHTML = 'Número do seu CEP: ' + '<b>' + cep.value + '</b>' + '<br>' + '. Rua: ' + '<b>' + JSON.stringify(urljson.logradouro) + '</b>' + '<br>' + '. Bairro: ' + '<b>' + JSON.stringify(urljson.bairro) + '</b>' + '<br>' + '. Cidade: ' + '<b>' + JSON.stringify(urljson.localidade) + '</b>' + '<br>' + '. Estado: ' + '<b>' + JSON.stringify(urljson.estado) + '</b>';
-                    //
-                }
-
-                else { campo.innerHTML = '' }
-            }
-            //
-
-            //Verificacao caso o input digitado esteja incorreto.
-            else {
-                if (campo.classList.contains('Ver')) {
-                    campo.innerHTML = "Talvez seu cep esteja errado:("
-                }
-                else { campo.innerHTML = '' }
-            }
-            //
-        }
-
-        //Uso do catch caso algo no codigo esteja incorreto.
-        catch (erro) {
-            if (campo.classList.contains('Ver')) {
-                campo.innerHTML = "Talvez seu cep esteja errado:("
-            }
-            else { campo.innerHTML = '' }
         }
         //
+
+        //Verificacao caso o input digitado esteja incorreto.
+        else { ExibirMensagem("Talvez seu cep esteja errado:(");}
+        //
     }
+
+    //Uso do catch caso algo no codigo esteja incorreto.
+    catch { ExibirMensagem("Ocorreu um erro ao buscar o CEP.");}
+    //
 }
 //function onclick 
 function Ver() {
-    campo.classList.toggle('Ver')
-    //Chama toda a function VerCep(), onde há o codigo principal.
-    VerCep()
-    //
+    campo.classList.toggle('Ver');
+    VerCep();
 }
 //
